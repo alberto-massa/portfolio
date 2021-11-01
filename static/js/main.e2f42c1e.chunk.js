7864ca2ccb9cd98693c55a19cce155b81947d5d8
Alberto Massa <massa.alberto@outlook.com>
2021-11-01 20:46:20 +0100

    Updates

---
+++ static/js/main.e2f42c1e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(9),n(0)),p=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"work in progress"})})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(p,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.e2f42c1e.chunk.js.map
